7e9e9a38073b5a95d5f520d98bcb0e2a62eaaf69
cod3gen <cod3gen@gmail.com>
2019-05-01 19:11:32 +0200

    Update local-storage.js

--- src/local-storage.js
+++ src/local-storage.js
@@ -30,6 +30,40 @@ export const removeItem = key => {
   } catch (e) {}
 }
 
+export const clearKeys = () => {
+	try {
+		const ls = getStorage()
+		for ( var i = 0, len = ls.length; i < len; ++i ) {
+			const item = ls.getItem(ls.key(i))
+			const cacheParsed = JSON.parse(cache)
+			const dateCache = cacheParsed.created
+			const timeExpire = cacheParsed.expiry
+			if (dateCache !== undefined && timeExpire !== undefined) {
+				let milisMultiplier = (1000 * 60) // default is in minutes unit
+
+				if (cacheParsed.unit || cacheParsed.unit === 's') { // in second unit
+				  milisMultiplier = (1000)
+				} else if (cacheParsed.unit || cacheParsed.unit === 'm') { // in minute unit
+				  milisMultiplier = (1000 * 60)
+				} else if (cacheParsed.unit || cacheParsed.unit === 'h') { // in hour unit
+				  milisMultiplier = (1000 * 60 * 60)
+				} else if (cacheParsed.unit || cacheParsed.unit === 'd') { // in day unit
+				  milisMultiplier = (1000 * 60 * 60 * 24)
+				}
+
+				const expiryInMilis = parseInt(timeExpire, 10) * milisMultiplier
+				const expiryTime = parseInt(dateCache, 10) + expiryInMilis
+
+				if (expiryTime < timeNow) {
+					__removeItem(ls, ls.key(i))
+				}
+			}
+			__getData(ls, ls.key(i))
+		}
+	} catch (e) {}
+	return null
+}
+
 export const clear = () => {
   try {
     const ls = getStorage()
